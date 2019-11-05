4269f0f5444eb54678ef2030164d368ab966eb6d
Magnus Ross <magnusaross@gmail.com>
2019-11-05 16:51:51 +0000

    rules functions now only act locally

--- boids/numba_boids_rules.py
+++ boids/numba_boids_rules.py
@@ -9,20 +9,19 @@ def get_co(x_all):
     for i in range(N):
         co += x_all[i]
     return co/N
-
+'''
 @numba.njit()
 def rule_com(i, pos_all, com, factor=0.0003):
     return (com - pos_all[i]) * factor
-
+'''
 @numba.njit()
-def rule_avoid(i, pos_all, radius=20, factor=0.001):
+def rule_avoid(i, pos_all, radius=10, factor=0.001):
     
     size = len(pos_all)
     pos_all = pos_all.astype(np.float64)
-    p = np.ones_like(pos_all[0])
+    p = np.zeros_like(pos_all[0])
 
     #for dealing with objects like walls the boids must avoid
- 
     rel_pos = pos_all - pos_all[i]
 
     for j in prange(size):
@@ -33,21 +32,60 @@ def rule_avoid(i, pos_all, radius=20, factor=0.001):
               
     return p * factor 
 
+
+@numba.njit()
+def rule_com(i, pos_all, radius=100, factor=0.001):
+    
+    size = len(pos_all)
+    pos_all = pos_all.astype(np.float64)
+    p = np.zeros_like(pos_all[0])
+    N = 0
+    #for dealing with objects like walls the boids must avoid
+    rel_pos = pos_all - pos_all[i]
+
+    for j in prange(size):
+        dist = np.power(rel_pos[j], 2)  
+        if i != j and np.sqrt(np.sum(dist))< radius:  
+            N += 1 
+            for k in range(len(p)):
+                p[k] += pos_all[j][k]
+    if N == 0:
+        return p
+    else:
+        return ((p/N) - pos_all[i]) * factor 
+
+@numba.njit()
+def rule_match(i, pos_all, vel_all, radius=10, factor=0.05):
+    
+    size = len(vel_all)
+    vel_all = vel_all.astype(np.float64)
+    p = np.zeros_like(vel_all[0])
+    N = 0
+    #for dealing with objects like walls the boids must avoid
+    rel_pos = pos_all - pos_all[i]
+
+    for j in prange(size):
+        dist = np.power(rel_pos[j], 2)  
+        if i != j and np.sqrt(np.sum(dist))< radius:  
+            N += 1 
+            for k in range(len(p)):
+                p[k] += vel_all[j][k]
+    if N == 0:
+        return p 
+    else:
+        return (-vel_all[i] + p/N ) * factor
+'''
 @numba.njit()
 def rule_match(i, vel_all, cov, factor=0.001):    
     return (vel_all[i] - cov) * factor
-
+'''
 @numba.njit()
 def update_boids(pos_all, vel_all):
-    
-    com = get_co(pos_all)
-    cov = get_co(vel_all)
 
     for i in prange(len(pos_all)):
-
-        v = rule_com(i, pos_all, com)
+        v = rule_com(i, pos_all)
         v += rule_avoid(i, pos_all)
-        v += rule_match(i, vel_all, cov)
+        v += rule_match(i, pos_all, vel_all)
 
         vel_all[i] += v 
         pos_all[i] += vel_all[i]

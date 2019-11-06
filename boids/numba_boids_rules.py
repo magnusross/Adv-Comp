ac86bc5eb76ec23602dace6a24fe2a768066ab7f
Magnus Ross <magnusaross@gmail.com>
2019-11-06 12:38:39 +0000

    add wall avoid functio

--- boids/numba_boids_rules.py
+++ boids/numba_boids_rules.py
@@ -9,11 +9,7 @@ def get_co(x_all):
     for i in range(N):
         co += x_all[i]
     return co/N
-'''
-@numba.njit()
-def rule_com(i, pos_all, com, factor=0.0003):
-    return (com - pos_all[i]) * factor
-'''
+
 @numba.njit()
 def rule_avoid(i, pos_all, radius=10, factor=0.001):
     
@@ -21,7 +17,6 @@ def rule_avoid(i, pos_all, radius=10, factor=0.001):
     pos_all = pos_all.astype(np.float64)
     p = np.zeros_like(pos_all[0])
 
-    #for dealing with objects like walls the boids must avoid
     rel_pos = pos_all - pos_all[i]
 
     for j in prange(size):
@@ -40,7 +35,6 @@ def rule_com(i, pos_all, radius=100, factor=0.001):
     pos_all = pos_all.astype(np.float64)
     p = np.zeros_like(pos_all[0])
     N = 0
-    #for dealing with objects like walls the boids must avoid
     rel_pos = pos_all - pos_all[i]
 
     for j in prange(size):
@@ -61,7 +55,6 @@ def rule_match(i, pos_all, vel_all, radius=10, factor=0.05):
     vel_all = vel_all.astype(np.float64)
     p = np.zeros_like(vel_all[0])
     N = 0
-    #for dealing with objects like walls the boids must avoid
     rel_pos = pos_all - pos_all[i]
 
     for j in prange(size):
@@ -74,18 +67,40 @@ def rule_match(i, pos_all, vel_all, radius=10, factor=0.05):
         return p 
     else:
         return (-vel_all[i] + p/N ) * factor
-'''
-@numba.njit()
-def rule_match(i, vel_all, cov, factor=0.001):    
-    return (vel_all[i] - cov) * factor
-'''
-@numba.njit()
-def update_boids(pos_all, vel_all):
 
+@numba.njit()
+def rule_wall_avoid(i, pos_all, pos_obj, radius=100, factor=10):
+    
+    size = len(pos_all)
+    pos_all = pos_all.astype(np.float64)
+    p = np.zeros_like(pos_all[0])
+    
+    rel_pos = pos_obj - pos_all[i]
+
+    for j in prange(size):
+        dist = np.power(rel_pos[j], 2)  
+        if i != j and np.sqrt(np.sum(dist)) < radius:  
+            for k in range(len(p)):
+                p[k] -= abs(rel_pos[j][k])
+    
+    return p * factor                
+
+
+@numba.njit()
+def update_boids(pos_all, vel_all,
+                 objs=0, pos_obj=np.empty((1,1))):
+    '''
+    objs option for numba compile 
+    '''
+    if objs == 1:
+        assert pos_all.shape[1] == pos_obj.shape[1]
+    
     for i in prange(len(pos_all)):
         v = rule_com(i, pos_all)
         v += rule_avoid(i, pos_all)
         v += rule_match(i, pos_all, vel_all)
+        if objs:
+            v += rule_wall_avoid(i, pos_all, pos_obj)
 
         vel_all[i] += v 
         pos_all[i] += vel_all[i]

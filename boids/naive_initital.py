7f6c5ff7eed6540da2eb6f3eec63128af1bb45d8
Magnus Ross <magnusaross@gmail.com>
2019-11-06 12:39:29 +0000

    test walls functionality

--- boids/naive_initital.py
+++ boids/naive_initital.py
@@ -1,20 +1,24 @@
 import numpy as np 
 import numba_boids_rules as b
 import tqdm
+from utilities import make_2D_square_walls
 
 
 
 #initalise boids
-N_b = 1000 
-pos_rand  = (2*np.random.rand(N_b, 2) - 1) * 100
-vel_rand  = np.flip(pos_rand, axis=1)*1e-3
+N_b = 300
+pos_rand  = (2*np.random.rand(N_b, 2) - 1) * 100 + np.array([300, 300])
+vel_rand  = (2*np.random.rand(N_b, 2) - 1) 
+
+walls = make_2D_square_walls(400, factor=3)
 
 
 #update N times 
-N = 400
+N = 700
 results = np.zeros((N, 2, N_b, 2))                                                 
 for j in tqdm.tqdm(range(N)):
-    results[j] = b.update_boids(pos_rand, vel_rand)
+    results[j] = b.update_boids(pos_rand, vel_rand, 
+                                objs=1, pos_obj=walls)
 np.save('results.npy', results)
 
 

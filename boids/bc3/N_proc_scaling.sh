db4c5715f151c5ea67a5d8c766ad29bd87f1c2cc
Magnus Ross <magnusaross@gmail.com>
2020-02-13 19:15:25 +0000

    new graphs

--- boids/bc3/N_proc_scaling.sh
+++ boids/bc3/N_proc_scaling.sh
@@ -5,7 +5,7 @@ export RS_DIR="./N_b_Scaling_RS/"
 
 
 mkdir $RS_DIR
-rm -f results*
+rm -f *.txt
 rm -f *.o*
 rm -f *.e*
 
@@ -39,7 +39,7 @@ mkdir $RS_DIR
 
 
 export MPI_PATH="./../calculation/bal_grid_mpi.py"
-python N_proc_scaling.py --rname grid_proc.txt --dir $RS_DIR --mpip $MPI_PATH
+python N_proc_scaling.py --rname bal_proc.txt --dir $RS_DIR --mpip $MPI_PATH
 
 for i in $RS_DIR*; do 
     for ((j=0; j<3; j++)); do
